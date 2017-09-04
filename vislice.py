f2c4c9c1d60ddd3df1b4707c2e55452fbf1821d8
Matevz Raspet <matevz.raspet@gmail.com>
2017-09-04 12:15:27 +0200

    funkcija za preverjanje crk, besede z veliko

--- vislice.py
+++ vislice.py
@@ -3,8 +3,8 @@ import time
 import random
 
 SIRINA = 9 # sirina mreze aplikacije
-ISKANE_BESEDE=["samokolnica","avtomobil","konjušnica","samozadostnost",
-               "obdarovanje","čebela","vrhunec","monopol","aktovka"]
+ISKANE_BESEDE=["SAMOKOLNICA","AVTOMOBIL","KONJUŠNICA","SAMOZADOSTNOST",
+               "OBDAROVANJE","ČEBELA","VRHUNEC","MONOPOL","AKOTVKA"]
 row_count = 0 # v kateri vrstici bodo novi gumbi
 def dobi_vrstico():
     global row_count
@@ -145,9 +145,6 @@ def v_lep_izpis(beseda_seznam):
         izpis += " " + beseda_seznam[x]
     return izpis
 
-def preveri_crko(crka):
-    print(crka)
-
 okno=tk.Tk()
 okno.title("Vislice")
 naslov=tk.Label(okno,text= "ČAS JE ZA VISLICE!!!", fg="blue").grid(row = dobi_vrstico(),columnspan = SIRINA)
@@ -157,23 +154,39 @@ naslov1 = tk.Label(okno, text = "Pozdravljeni v igrici Zabavne vislice.\n"
 
 izrecene_crke =''
 trenutna_beseda = ISKANE_BESEDE[0] #prototip
-prikazan_text = inicializacija_prikaza(len(trenutna_beseda))
+array_beseda = inicializacija_prikaza(len(trenutna_beseda))
 stevilo_poskusov = 0 #možnih poskusov je 10
 crke=["A","B","C","Č","D","E","F","G","H","I","J","K","L","M","N","O","P","R",
           "S","Š","T","U","V","Z","Ž"]
 
 podnaslov=tk.Label(okno,text= "Izberi črko:").grid(row = dobi_vrstico(),columnspan = SIRINA)
 
+def preveri_crko(crka):
+    global izrecene_crke
+    global trenutna_beseda
+    global array_beseda
+
+    if crka in izrecene_crke:
+        return
+    izrecene_crke += crka
+    if crka not in trenutna_beseda:
+        print("DEBUG ni v besedi")
+        return
+    
+    for x in range(len(trenutna_beseda)):
+        if(crka == trenutna_beseda[x]):
+            array_beseda[x] = crka
+
 # dodam gumbe za crke
 zacetek_gumbov = dobi_vrstico()
 for x in range(len(crke)):
-    tk.Button(okno,text = crke[x], command = lambda crka=crke[x]: preveri_crko(crka))
-    .grid(row = x // SIRINA + zacetek_gumbov,column = x % SIRINA) # problemi z lambda
+    tk.Button(okno,text = crke[x], command = lambda crka=crke[x]: preveri_crko(crka)).grid(row = x // SIRINA
+                        + zacetek_gumbov,column = x % SIRINA) # problemi z lambda
 row_count = zacetek_gumbov + len(crke)//SIRINA + 1 # nastavim da row_count kaze na pravilno vrstico
 
 tk.Label(okno, text = "Iskana beseda:").grid(row = dobi_vrstico(),columnspan = SIRINA)
 
-izpis_lab = tk.Label(okno, text = v_lep_izpis(prikazan_text),font = 2000).grid(row = dobi_vrstico(),
+izpis_lab = tk.Label(okno, text = v_lep_izpis(array_beseda),font = 2000).grid(row = dobi_vrstico(),
                                                                                columnspan = SIRINA)
 
 
