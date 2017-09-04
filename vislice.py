aa847a44622483f11778eb099a1024f26d815c23
Matevz Raspet <matevz.raspet@gmail.com>
2017-09-04 13:12:57 +0200

    zmaga ali poraz

--- vislice.py
+++ vislice.py
@@ -174,27 +174,44 @@ izpis = tk.StringVar()
 izpis.set(v_lep_izpis(array_beseda))
 izpis_lab = tk.Label(okno, textvariable = izpis,font = 2000).grid(row = dobi_vrstico(),
                                                                          columnspan = SIRINA)
+# visl_text[0] -> string ki se prikaze, slika vislic
+# visl_text [1] -> koliko napacnih izbir je ze bilo
 visl_text = [tk.StringVar(),0]
 visl_text[0].set(HANGMAN[0])
 vislica = tk.Message(okno, textvariable = visl_text[0],font = 2000).grid(row = dobi_vrstico(),
                                                                          columnspan = SIRINA)
+govor = tk.StringVar()
+tk.Label(okno, textvariable = govor,font = 2000).grid(row = dobi_vrstico(),
+                                                                columnspan = SIRINA)
 def preveri_crko(crka):
     global izrecene_crke
     global trenutna_beseda
     global array_beseda
     global visl_text
 
+    if visl_text[1] >= len(HANGMAN)-1 or '_' not in v_lep_izpis(array_beseda):
+        return
+    
     if crka in izrecene_crke:
+        govor.set("Crko " + str(crka) + " ste ze pritisnili.")
         return
     izrecene_crke += crka
     if crka not in trenutna_beseda:
         visl_text[1] += 1
+        if visl_text[1] >= len(HANGMAN)-1:
+            govor.set("Zal ste izgubili. Konec igre.")
+            visl_text[0].set(HANGMAN[visl_text[1]])
+            return
         visl_text[0].set(HANGMAN[visl_text[1]])
+        govor.set("Crka " + str(crka) + " ni v iskani besedi.")
         return
     
     for x in range(len(trenutna_beseda)):
         if(crka == trenutna_beseda[x]):
             array_beseda[x] = crka
     izpis.set(v_lep_izpis(array_beseda))
+    govor.set("Crka " + str(crka) + " je v iskani besedi.")
 
+    if '_' not in v_lep_izpis(array_beseda):
+        govor.set("Zmagali ste.")
 
